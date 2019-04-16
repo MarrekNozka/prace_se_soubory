6c3d4fc6f6be6fdae597c9c767b16bebed71bc0f
romansvozil <romdrak@email.cz>
2019-04-16 23:05:27 +0200

    Plne funkcni program! uz jen mozna dodelat readme, ale to pocka

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import random
 import os
 
 # options
-IGNORE_CHARS = (" ")
+IGNORE_CHARS = (" \n")
 HLASKY = ["aeiyou","qwrtpsdfghjklzxcvbnm"]
 
 # prevod na mala pismena, jako vstup string textu
@@ -42,7 +42,7 @@ def nahodny(pocet=0):
     except ValueError:
         return ""
     text = ""
-    for _ in range(pocet):
+    for slovo in range(pocet):
         typ = random.randint(0, 1) # 0 - samohhlaska, 1 - souhlaska
         delka_slova = random.randint(1, 8)
         for _ in range(delka_slova):
@@ -51,7 +51,11 @@ def nahodny(pocet=0):
                 hlaska = hlaska.upper()
             text += hlaska
             typ = not(typ)
-        text += " "
+        if slovo % 10 == 0:
+            text += "\n"
+        else:
+            text += " "
+
     return text
 
 # uloz list textu
@@ -59,15 +63,68 @@ def uloz(soubor, text):
     with open(soubor, "w") as f:
         for line in text:
             f.write(line)
-            
+
 # vypise slovnik jako tabulku
 def vypis(slovnik):
+    mx = max(slovnik.values())
+    koeficient = 20 / mx
     print("{:<8} {:<8}".format('Znak','Pocet'))
     for item in slovnik:
-        print("{:<8} {:<8}".format(item,slovnik[item]))
-        
-# testy 
-"""               
+        mrizky = round(koeficient * slovnik[item]) * "#"
+        print("{:<8} {:<8} {:<20}".format(item,slovnik[item], mrizky))
+
+# programy
+# 1 zmensi
+def jedna():
+    nazev = input("Nazev souboru: ")
+    novy_soubor = []
+    with open(nazev, "r") as f:
+        line = f.readline()
+        while line:
+            novy_soubor.append(zmensi(line))
+            line = f.readline()
+        uloz(nazev, novy_soubor)
+        print("Soubor {0} uspesne ulozen! ".format(nazev))
+
+# 2 nahradi
+def dva():
+    nazev = input("Nazev souboru: ")
+    co = input("Jake slovo: ")
+    cim = input("Jakym slovem: ")
+    novy_soubor = []
+    with open(nazev, "r") as f:
+        line = f.readline()
+        while line:
+            novy_soubor.append(nahrad(line, co, cim))
+            line = f.readline()
+        uloz(nazev, novy_soubor)
+        print("Soubor {0} uspesne ulozen! ".format(nazev))
+
+# 3 statistika
+def tri():
+    nazev = input("Nazev souboru: ")
+    with open(nazev, "r") as f:
+        stat = {}
+        line = f.readline()
+        while line:
+            stat = statistika(line, stat)
+            line = f.readline()
+        vypis(stat)
+# 4 nahodny text
+def ctyri():
+    nazev = input("Nazev souboru i s priponou: ")
+    while True:
+        try:
+            pocet = int(input("Pocet nahodnych slov: "))
+            break
+        except ValueError:
+            continue
+    with open(nazev, "w") as f:
+        f.write(nahodny(pocet))
+    print("Soubor {0} uspesne ulozen! ".format(nazev))
+
+# testy
+"""
 text = nahodny()
 print("TESTY")
 print(text)
@@ -79,9 +136,23 @@ print(nahrad(text, "a", "/jahoda/"))
 
 print(
       """
+===============================================================================
+ ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄       ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌     ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+ ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀  ▐░▌   ▐░▌  ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌
+     ▐░▌     ▐░▌            ▐░▌ ▐░▌       ▐░▌     ▐░▌       ▐░▌▐░▌       ▐░▌
+     ▐░▌     ▐░█▄▄▄▄▄▄▄▄▄    ▐░▐░▌        ▐░▌     ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌
+     ▐░▌     ▐░░░░░░░░░░░▌    ▐░▌         ▐░▌     ▐░▌       ▐░▌▐░░░░░░░░░░░▌
+     ▐░▌     ▐░█▀▀▀▀▀▀▀▀▀    ▐░▌░▌        ▐░▌     ▐░▌       ▐░▌▐░█▀▀▀▀█░█▀▀
+     ▐░▌     ▐░▌            ▐░▌ ▐░▌       ▐░▌     ▐░▌       ▐░▌▐░▌     ▐░▌
+     ▐░▌     ▐░█▄▄▄▄▄▄▄▄▄  ▐░▌   ▐░▌      ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░▌      ▐░▌
+     ▐░▌     ▐░░░░░░░░░░░▌▐░▌     ▐░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░▌       ▐░▌
+      ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀       ▀       ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀
+===============================================================================
+
     1) Převod na malá písmena
     2) Nahrazení znaků
-    3) Statistika souboru 
+    3) Statistika souboru
     4) Generování náhodného textu
     5) Konec
       """)
@@ -93,56 +164,28 @@ while True:
     except ValueError:
         print("Neplatna moznost! ")
         continue
-    if vstup in range(1,3):
+    if vstup == 1 or vstup == 2:
         try:
-            nazev = input("Nazev souboru: ")
-            if vstup == 2:
-                co = input("Jake slovo: ")
-                cim = input("Jakym slovem: ")
-            novy_soubor = []
-            with open(nazev, "r") as f:
-                line = f.readline()
-                while line:
-                    if vstup == 1:
-                        novy_soubor.append(zmensi(line))
-                    elif vstup == 2:
-                        novy_soubor.append(nahrad(line, co, cim))
-                    line = f.readline()
-                uloz(nazev, novy_soubor)
-                print("Soubor {0} uspesne ulozen! ".format(nazev))
+            if vstup == 1:
+                jedna()
+            else:
+                dva()
         except FileNotFoundError:
             print("Soubor nenalezen! ")
             continue
-        
+
     elif vstup == 3:
         try:
-            nazev = input("Nazev souboru: ")
-            with open(nazev, "r") as f:
-                stat = {}
-                line = f.readline()
-                while line:
-                    stat = statistika(line, stat)
-                    line = f.readline()
-                vypis(stat)
+            tri()
         except FileNotFoundError:
             print("Soubor nenalezen! ")
             continue
-        
+
     elif vstup == 4:
-        nazev = input("Nazev souboru i s priponou: ")
-        while True:
-            try:
-                pocet = int(input("Pocet nahodnych slov: "))
-                break
-            except ValueError:
-                continue
-        with open(nazev, "w") as f:
-            f.write(nahodny(pocet))
-        print("Soubor {0} uspesne ulozen! ".format(nazev))
+        ctyri()
 
     elif vstup == 5:
         exit(0)
-        
-    else: 
+
+    else:
         print("Neplatna moznost! ")
-        
