5a1ad4e83eeeb01b47d53a8808fb457301107a97
Roman Svozil <svo35103@spseol.cz>
2019-04-16 11:22:56 +0200

    Temer plne funkcni, chybi uz jen u statistiky dodelat "grafiku" v podobe #####.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Created on Mon Apr 15 08:25:24 2019
 """
 # importy
 import random
+import os
 
 # options
 IGNORE_CHARS = (" ")
@@ -20,10 +21,10 @@ def zmensi(text):
 def nahrad(text, co, cim):
     return text.replace(co, cim)
 
-# vrati slovnik a jako vstup bere string text
-def statistika(text):
-    statistika = {}
+# vrati slovnik a jako vstup bere string text a slovnik predchozi statistiky, popripade prazdny
+def statistika(text, statistika={}):
     for znak in text:
+        znak = znak.upper()
         if znak in IGNORE_CHARS:
             continue
         if znak in statistika:
@@ -42,7 +43,7 @@ def nahodny(pocet=0):
         return ""
     text = ""
     for _ in range(pocet):
-        typ = random.randint(0, 1) # 1 - samohhlaska, 2 - souhlaska
+        typ = random.randint(0, 1) # 0 - samohhlaska, 1 - souhlaska
         delka_slova = random.randint(1, 8)
         for _ in range(delka_slova):
             hlaska = random.choice(HLASKY[typ])
@@ -53,14 +54,27 @@ def nahodny(pocet=0):
         text += " "
     return text
 
-# testy                
+# uloz list textu
+def uloz(soubor, text):
+    with open(soubor, "w") as f:
+        for line in text:
+            f.write(line)
+            
+# vypise slovnik jako tabulku
+def vypis(slovnik):
+    print("{:<8} {:<8}".format('Znak','Pocet'))
+    for item in slovnik:
+        print("{:<8} {:<8}".format(item,slovnik[item]))
+        
+# testy 
+"""               
 text = nahodny()
 print("TESTY")
 print(text)
 print(statistika(text))
 print(zmensi(text))
 print(nahrad(text, "a", "/jahoda/"))
-
+"""
 # menu
 
 print(
@@ -72,10 +86,63 @@ print(
     5) Konec
       """)
 
+
 while True:
-    vstup = input(">>> ", end="")
-    if vstup == "1":
-        nazev = input("Název souboru: ", end="" )
-        with open(nazev, "r") as f:
-            pass
+    try:
+        vstup = int(input(">>> "))
+    except ValueError:
+        print("Neplatna moznost! ")
+        continue
+    if vstup in range(1,3):
+        try:
+            nazev = input("Nazev souboru: ")
+            if vstup == 2:
+                co = input("Jake slovo: ")
+                cim = input("Jakym slovem: ")
+            novy_soubor = []
+            with open(nazev, "r") as f:
+                line = f.readline()
+                while line:
+                    if vstup == 1:
+                        novy_soubor.append(zmensi(line))
+                    elif vstup == 2:
+                        novy_soubor.append(nahrad(line, co, cim))
+                    line = f.readline()
+                uloz(nazev, novy_soubor)
+                print("Soubor {0} uspesne ulozen! ".format(nazev))
+        except FileNotFoundError:
+            print("Soubor nenalezen! ")
+            continue
+        
+    elif vstup == 3:
+        try:
+            nazev = input("Nazev souboru: ")
+            with open(nazev, "r") as f:
+                stat = {}
+                line = f.readline()
+                while line:
+                    stat = statistika(line, stat)
+                    line = f.readline()
+                vypis(stat)
+        except FileNotFoundError:
+            print("Soubor nenalezen! ")
+            continue
+        
+    elif vstup == 4:
+        nazev = input("Nazev souboru i s priponou: ")
+        while True:
+            try:
+                pocet = int(input("Pocet nahodnych slov: "))
+                break
+            except ValueError:
+                continue
+        with open(nazev, "w") as f:
+            f.write(nahodny(pocet))
+        print("Soubor {0} uspesne ulozen! ".format(nazev))
+
+    elif vstup == 5:
+        exit(0)
+        
+    else: 
+        print("Neplatna moznost! ")
         
